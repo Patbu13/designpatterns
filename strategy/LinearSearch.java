a81c1c94d769b5dfd94e5b94a2d07652520a1e16
Patrick Burroughs <burrougp@email.sc.edu>
2021-08-29 18:34:35 -0400

    setup with methods constructed and commented before leaving for chapter

---
+++ strategy/LinearSearch.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+/**
+ * Patrick Burroughs
+ * Portia Plante 247 Section 002
+ * Strategy Design Pattern
+*/
+
+/**
+ * One of two forms of searching the given list for the given string
+ * 
+ * Searches entire list one by one
+ */
+public class LinearSearch {
+
+    /**
+     * Method for checking whether the given list contains the given string
+     * 
+     * @param data list being searched through for the string
+     * @param item string being searched for
+     * @return boolean returning whether the list contains the given string
+     */
+    public boolean contains(ArrayList<String> data, String item)    {
+        return true;
+    }
+
+}
